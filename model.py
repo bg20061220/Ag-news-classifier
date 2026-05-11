252c6abec59641a7ce866bd84fba84090971f775
bg20061220 <goel.bhavya020@gmail.com>
2026-05-10 22:04:36 -0400

    wrote the training loop

--- model.py
+++ model.py
@@ -9,3 +9,20 @@ class NewsClassifier(nn.Module):
         self.fc1 = nn.Linear(embedding_dim , hidden_dim) 
         self.relu = nn.Relu()
         self.fc2 = nn.Linear(hidden_dim , num_classes) 
+
+    def forward(self , x): 
+        embedded = self.embedding(x) 
+        pooled = embedded.mean(dim = 1)
+        out = self.fc1(pooled) 
+        out = self.relu(out)
+        out = self.fc2(out) 
+        return out 
+
+model = NewsClassifier(
+    vocab_size=len(vocab),
+    embedding_dim=64, 
+    hidden_dim=128,
+    num_classes=4
+) 
+
+print(model)
---
+++ train.py
@@ -0,0 +1,32 @@
+import torch 
+import torch.nn as nn 
+from dataset import train_loader 
+from model import model 
+
+optimzer = torch.optim.Adam(model.parameters() , lr = 0.001) 
+criterion = nn.CrossEntropyLoss() 
+
+def train(num_epochs = 10): 
+    for epoch in range(num_epochs):
+        model.train() 
+        total_loss = 0 
+        correct = 0 
+        total = 0 
+
+        for texts , labels in train_loader(): 
+            predictions = model(texts) 
+            loss = criterion(predictions , labels) 
+
+            optimzer.zero_grad() 
+            loss.backward()
+            optimzer.step() 
+
+            total_loss += loss.item() 
+            predicted_classes = predictions.argmax(dim=1)
+            correct += (predicted_classes == labels).sum().item()
+            total += labels.size(0)
+
+    accuracy = correct/total
+    print(f"Epoch {epoch+1}: Loss={total_loss:.3f} Accuracy={accuracy:.3f}")
+
+train() 
